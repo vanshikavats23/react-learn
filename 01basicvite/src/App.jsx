df387d0485dcc6a506f4032aad543186d978a7f1
vanshikavats23 <144532373+vanshikavats23@users.noreply.github.com>
2026-01-23 21:04:46 +0530

    evaluated exp

--- 01basicvite/src/App.jsx
+++ 01basicvite/src/App.jsx
@@ -1,12 +1,14 @@
 
 import Vanshi from "./vanshi"
 function App() {
-
+ //js
+ const username=" vanshika vats"
 
   return (
     //fragement, coz react returns single element
     <>
-      <h1>Hiii</h1>
+    //{} is called evaluated expression
+      <h1>Hiii {username}</h1>
       <Vanshi />
 
     </>
