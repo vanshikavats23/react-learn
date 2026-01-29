97132778d69f043b44ca5b2a7eec590fe13694bb
vanshikavats23 <144532373+vanshikavats23@users.noreply.github.com>
2026-01-29 22:54:56 +0530

    tailwind

---
+++ 04tailwindprops/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline bg-blue-500 text-white p-4 rounded-xl">HIII TAILWIND TESTING</h1>
+      
+    </>
+  )
+}
+
+export default App
