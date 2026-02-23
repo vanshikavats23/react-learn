34ad86a0bc420322bb5d3153b876079e3f62a286
vanshikavats23 <144532373+vanshikavats23@users.noreply.github.com>
2026-02-23 16:53:04 +0530

    a

---
+++ CURRENCY CONVERTOR/src/components/inputBox.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+function InputBox({
+    label,
+    amount,
+    onAmountChange,
+    onCurrencyChange,
+    currencyOptions = [],
+    selectCurrency = "usd",
+    amountDisable = false,
+    currencyDisable = false,
+    className = "",
+}) 
+
+export default InputBox;
